56fa87a58f5d675d2f6101245a0590a751055b5d
GithubObjectPharm <ausaafpharmacyprep@gmail.com>
2026-01-05 19:27:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -475,6 +475,10 @@ def serve_upload(filename):
 
 
 # -------------------- Run --------------------
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=5002,
+        debug=False
+    )
+
